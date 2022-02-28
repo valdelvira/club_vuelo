75fc12fa3a47927643c4f7085ea9362fb29ade44
Diego V <egovaldel@gmail.com>
2022-02-28 18:51:35 +0100

    solucion conflictos

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -3,15 +3,7 @@ const router = require("express").Router()
 router.use("/user", require('./user.routes'))
 router.use("/events", require('./events.routes'))
 router.use("/news", require('./news.routes'))
-<<<<<<< HEAD
-// router.use("/auth", './auth.routes')
-=======
 router.use("/auth", require('./auth.routes'))
-<<<<<<< HEAD
->>>>>>> b4252312f4c668c761a9116ee1cc5c427634f22f
-=======
->>>>>>> refs/remotes/origin/main
->>>>>>> 41859f1ec6760b29dd06eb449c04c806651b7dc0
 
 
 module.exports = router
