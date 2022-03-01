b9c391882b355fd6c70dba6c3756105d90d01be0
Miguel Ortega Ward <miguel.owd@gmail.com>
2022-03-01 16:34:37 +0100

    cambio miguel

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -3,11 +3,15 @@ import axios from 'axios'
 class AuthService {
 
     constructor() {
-        this.api = axios.create({ baseURL: `${process.env.REACT_APP_API_URL}/auth`})
+        this.api = axios.create({ baseURL: `${process.env.REACT_APP_API_URL}/auth` })
     }
 
     signup(credentials) {
         return this.api.post('/signup', credentials)
+    }
+
+    login(credentials) {
+        return this.api.post('/login', credentials)
     }
 }
 
