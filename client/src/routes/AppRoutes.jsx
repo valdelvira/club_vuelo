6d07bd7b8f8b204a367a10e2471da836cdf07040
Mario Diaz <mariodzls@gmail.com>
2022-03-01 16:34:02 +0100

    Cambios Mario

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import IndexPage from '../pages/IndexPage/IndexPage'
+import SignUpPage from "../pages/SignUpPage/SignUpPage"
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+
+            <Route path="/" element={<IndexPage />}/>
+            <Route path="/signup" element={<SignUpPage/>}/>
+            <Route path="*" element={<h1>404</h1>}/>
+
+        </Routes>
+    )
+}
+
+export default AppRoutes
