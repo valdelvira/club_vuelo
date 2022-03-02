7db621686f47062278710915622762c32477352f
Mario Diaz <mariodzls@gmail.com>
2022-03-02 18:27:44 +0100

    Hds

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -20,25 +20,6 @@ const Navigation = () => {
                         <NavDropdown title="Akaflieg Madrid" id="navbarScrollingDropdown">
                             <NavLink to="/about-us">
                                 <NavDropdown.Item as="span">Quiénes somos</NavDropdown.Item>
-<<<<<<< HEAD:client/src/components/Navigator/Navigator.jsx
-=======
-                            </NavLink>
-
-                            <NavLink to="/projects">
-                                <NavDropdown.Item as="span">Proyectos</NavDropdown.Item>
-                            </NavLink>
-
-                            <NavLink to="/sponsors">
-                                <NavDropdown.Item as="span">Patrocinadores</NavDropdown.Item>
-                            </NavLink>
-
-                            <NavLink to="/prices">
-                                <NavDropdown.Item as="span">Precios</NavDropdown.Item>
-                            </NavLink>
-
-                            <NavLink to="/contact">
-                                <NavDropdown.Item as="span">Contacto</NavDropdown.Item>
->>>>>>> b95a468b868f51a19bfbbde8e296af107075f3db:client/src/components/Navigation/Navigation.jsx
                             </NavLink>
                         </NavDropdown>
 
