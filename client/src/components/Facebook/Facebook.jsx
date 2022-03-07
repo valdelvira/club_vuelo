fc9bcf49253b48ceffb81a5125d7b5c18b5346fa
Mario Diaz <mariodzls@gmail.com>
2022-03-07 16:18:26 +0100

    Facebook

---
+++ client/src/components/Facebook/Facebook.jsx
@@ -0,0 +1,13 @@
+
+
+const Facebook = () => {
+    return (
+        <div class="fb-page" data-href="https://www.facebook.com/clubvueloupm" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+            <blockquote cite="https://www.facebook.com/clubvueloupm" class="fb-xfbml-parse-ignore">
+                <a href="https://www.facebook.com/clubvueloupm">Club de Vuelo UPM</a>
+            </blockquote>
+        </div>
+    )
+}
+
+export default Facebook
